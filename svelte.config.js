6b66119fb41bb321ba6822212fc630f9cb49de54
Lea <s1953043@ed.ac.uk>
2025-07-21 16:52:11 +0100

    Add footnotes, header, footer

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,9 @@
-import { vismarkMdsvex } from 'vismark/config';
+import { vismarkConfig } from 'vismark/config';
 import adapter from '@sveltejs/adapter-auto';
+import { server } from 'typescript';
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = vismarkMdsvex({
+const config = vismarkConfig({
 	kit: { // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 	// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 	// See https://svelte.dev/docs/kit/adapters for more information about adapters.
