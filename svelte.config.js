c771cefc6d7e689627e26146585b551f226bdc5e
Lea <s1953043@ed.ac.uk>
2025-08-11 16:01:42 +0100

    Resolve routes

--- svelte.config.js
+++ svelte.config.js
@@ -19,7 +19,8 @@ const config = vismarkConfig({
 			strict: true
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH,
+			relative: true
 		}
 	}
 });
