33de10dff0fecbfc4dab9502717167e363bfab2b
Lea <s1953043@ed.ac.uk>
2025-08-11 15:42:21 +0100

    Attempt deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import { vismarkConfig } from 'vismark/config';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
+// import adapter from '@sveltejs/adapter-auto';
 import { server } from 'typescript';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -7,7 +8,19 @@ const config = vismarkConfig({
 	kit: { // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 	// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 	// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		appDir: 'app', // Required as the default is _app	
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: '404.html',
+			precompress: false,
+			strict: true
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
 	}
 });
 
